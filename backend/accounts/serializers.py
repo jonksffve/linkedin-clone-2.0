bd992b898d56a370ecdb7bb756347934ee6add19
Jonathan Ramirez <jonksff@gmail.com>
2023-07-20 11:08:38 -0400

    EDIT: usercreation hashing password on create
    ADD: userserializer for getting user information

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -7,6 +7,31 @@ class UserCreationSerializer(serializers.ModelSerializer):
     Serializer used for creation of CustomUser model instances
     """
 
+    def create(self, validated_data):
+        """
+        Create a custom user object and properly hash the password
+        """
+        password = validated_data["password"]
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
     class Meta:
         model = CustomUser
         fields = ["first_name", "last_name", "email", "password", "avatar"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer used to retrieve information about a CustomUser model instance
+    """
+
+    name = serializers.SerializerMethodField()
+
+    def get_name(self, obj):
+        return obj.get_full_name()
+
+    class Meta:
+        model = CustomUser
+        fields = ["id", "name", "email", "avatar"]
