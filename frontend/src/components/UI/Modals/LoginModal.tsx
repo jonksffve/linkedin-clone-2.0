c352dbb7cafebc8e1b6687c2dd879cb394a6297b
Jonathan Ramirez <jonksff@gmail.com>
2023-07-20 15:38:33 -0400

    ADD: new loginmodal component

---
+++ frontend/src/components/UI/Modals/LoginModal.tsx
@@ -0,0 +1,32 @@
+import { useCallback, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import LoginForm from '../Auth/Forms/LoginForm';
+import Header from '../Auth/Header';
+import SocialsMinimized from '../Auth/SocialsMinimized';
+import BaseModal from './BaseModal';
+import { uiActions } from '../../../store/slices/ui-slice';
+
+const LoginModal = () => {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const dispatch = useAppDispatch();
+	const uiState = useAppSelector((state) => state.ui);
+
+	const handleClose = useCallback(() => {
+		dispatch(uiActions.onCloseLoginModal());
+	}, [dispatch]);
+
+	return (
+		<BaseModal
+			onShow={uiState.showLoginModal}
+			disabled={isLoading}
+			onClose={handleClose}
+		>
+			<Header title='Login information' />
+			<LoginForm />
+			<SocialsMinimized />
+		</BaseModal>
+	);
+};
+
+export default LoginModal;
