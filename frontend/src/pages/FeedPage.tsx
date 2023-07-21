5239da5649ea104c7d46b933db8e818c9a248cdd
Jonathan Ramirez <jonksff@gmail.com>
2023-07-21 14:58:27 -0400

    ADD: all components layout

--- frontend/src/pages/FeedPage.tsx
+++ frontend/src/pages/FeedPage.tsx
@@ -1,13 +1,30 @@
-import FeedContent from '@/components/Feed/FeedContent';
+import ConnectionsSummary from '@/components/Feed/ConnectionsSummary';
+import PostContent from '@/components/Feed/PostContent';
+import PostCreation from '@/components/Feed/PostCreation';
+import ProfileSummary from '@/components/Feed/ProfileSummary';
+import Spinner from '@/components/UI/Spinner';
 
 const FeedPage = () => {
 	return (
 		<div className='flex flex-row gap-4 h-full relative'>
-			<div className='w-1/4'>1</div>
-			<div className='w-2/4 max-h-screen overflow-auto'>
-				<FeedContent />
+			<div className='w-1/4 '>
+				<ProfileSummary />
 			</div>
-			<div className='w-1/4 sticky max-h-full'>3</div>
+			<div className='w-2/4 overflow-auto relative'>
+				<div className='p-2'>
+					<PostCreation />
+					<Spinner />
+					{/* <PostContent />
+					<PostContent />
+					<PostContent />
+					<PostContent />
+					<PostContent />
+					<PostContent /> */}
+				</div>
+			</div>
+			<div className='w-1/4'>
+				<ConnectionsSummary />
+			</div>
 		</div>
 	);
 };
