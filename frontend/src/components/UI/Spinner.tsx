6a21f5789081c0d645d05d59eb6b75a21ff64167
Jonathan Ramirez <jonksff@gmail.com>
2023-07-21 14:57:56 -0400

    ADD: mask param so we determine hide parent

--- frontend/src/components/UI/Spinner.tsx
+++ frontend/src/components/UI/Spinner.tsx
@@ -1,9 +1,19 @@
-const Spinner = () => {
+interface SpinnerProps {
+	mask?: boolean;
+}
+
+/**
+ * Loading spinner animation
+ *
+ * @param mask Determines weather renders a mask on top of the parent component
+ */
+const Spinner: React.FC<SpinnerProps> = ({ mask = false }) => {
 	return (
 		<>
-			<div className='absolute w-full h-full bg-neutral-200/60 top-0 left-0 cursor-not-allowed rounded-md'></div>
-			<div className='loader cursor-not-allowed fixed top-[calc(50%-40px)] left-[calc(50%-40px)] translate-x-[50%] translate-y-[-50%]'></div>
-			;
+			{mask && (
+				<div className='absolute w-full h-full bg-neutral-200/40 top-0 left-0 cursor-not-allowed rounded-md z-10'></div>
+			)}
+			<div className='loader fixed top-[calc(35%-40px)] left-[calc(50%-40px)] translate-x-[50%] translate-y-[-50%] '></div>
 		</>
 	);
 };
