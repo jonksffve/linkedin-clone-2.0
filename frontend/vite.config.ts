86b6a1527e1059aec5393134781683c080e30da7
Jonathan Ramirez <jonksff@gmail.com>
2023-07-21 08:15:02 -0400

    ADD: alias for long imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// @see https://dev.to/tilly/aliasing-in-vite-w-typescript-1lfo
 export default defineConfig({
-  plugins: [react()],
-})
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@api': path.resolve(__dirname, './src/api'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@components': path.resolve(__dirname, './src/components'),
+		},
+	},
+	plugins: [react()],
+});
