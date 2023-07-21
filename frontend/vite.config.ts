c7b4b802659381c733ac0f4dcbe6ef2ec793a818
Jonathan Ramirez <jonksff@gmail.com>
2023-07-21 08:21:40 -0400

    ADD: alias for less typing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
-			'@api': path.resolve(__dirname, './src/api'),
 			'@assets': path.resolve(__dirname, './src/assets'),
 			'@components': path.resolve(__dirname, './src/components'),
 		},
