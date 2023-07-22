9e03735c9011abd0db6d8cbb99753ce8eebdef47
Jonathan Ramirez <jonksff@gmail.com>
2023-07-22 12:00:09 -0400

    EDIT: url naming for reverse

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,19 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("account/", views.UserCreationView.as_view()),
-    path("account/profile/", views.UserRetrieveInformationView.as_view()),
-    path("login/", views.LoginView.as_view()),
+    path("account/", views.UserCreationView.as_view(), name="register"),
+    path(
+        "account/profile/",
+        views.UserRetrieveInformationView.as_view(),
+        name="profile",
+    ),
+    path("login/", views.LoginView.as_view(), name="login"),
     path("auth/", include("knox.urls")),
+    path("post/", views.PostListCreateView.as_view(), name="post-list-create"),
+    path("post/like/", views.PostLikeCreateView.as_view(), name="post-like-create"),
+    path(
+        "post/like/<str:post>/",
+        views.PostLikeDestroyView.as_view(),
+        name="post-like-destroy",
+    ),
 ]
