2decbca7cdc0400dd86b26fa3e354e00169fc599
Jonathan Ramirez <jonksff@gmail.com>
2023-07-22 11:53:55 -0400

    ADD: new test organization

---
+++ backend/api/tests/test_views.py
@@ -0,0 +1,491 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from django.urls import reverse
+from django.db import IntegrityError
+
+from accounts.models import CustomUser
+
+from feed.models import Post, PostLike
+
+
+class UserCreationViewTest(APITestCase):
+    """
+    Test cases for User model creation view
+
+    Returns:
+        - 201 Created: with a serialized representation of the user object
+        - 400 Bad request: if ValidationError was raised or IntegrityError
+    """
+
+    def setUp(self):
+        self.url = reverse("register")
+        self.user = CustomUser.objects.create_user(
+            email="testuser@site.com",
+            password="testpass",
+            first_name="Testing",
+            last_name="DRF",
+        )
+
+    def test_create_user_valid_data(self):
+        data = {
+            "email": "test@example.com",
+            "password": "testpassword",
+            "first_name": "Testing",
+            "last_name": "User DRF",
+        }
+
+        response = self.client.post(self.url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CustomUser.objects.count(), 2)
+        self.assertEqual(len(response.data), 8)
+        self.assertEqual(response.data["email"], "test@example.com")
+        # password needs to be hashed
+        self.assertNotEqual(response.data["password"], "testpassword")
+        self.assertIsNotNone(response.data["avatar"])
+        self.assertIsNotNone(response.data["banner"])
+
+    def test_create_user_invalid_data(self):
+        # Test case where invalid data causes a ValidationError (no data)
+        invalid_data = {}
+        response = self.client.post(self.url, invalid_data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 4)
+        self.assertIn("first_name", response.data)
+        self.assertIn("last_name", response.data)
+        self.assertIn("email", response.data)
+        self.assertIn("password", response.data)
+
+        # Test case where invalid data causes a ValidationError (no first_name)
+        invalid_data = {
+            "last_name": "User",
+            "email": "test@user.com",
+            "password": "1234567",
+        }
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("first_name", response.data)
+
+        # Test case where invalid data causes a ValidationError (no last_name)
+        invalid_data = {
+            "first_name": "Testing",
+            "email": "test@user.com",
+            "password": "1234567",
+        }
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("last_name", response.data)
+
+        # Test case where invalid data causes a ValidationError (no email)
+        invalid_data = {
+            "first_name": "Testing",
+            "last_name": "User",
+            "password": "1234567",
+        }
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("email", response.data)
+
+        # Test case where invalid data causes a ValidationError (no password)
+        invalid_data = {
+            "first_name": "Testing",
+            "last_name": "User",
+            "email": "test@user.com",
+        }
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("password", response.data)
+
+        # Test case where invalid data causes a IntegrityError (same email)
+        invalid_data = {
+            "first_name": "Testing",
+            "last_name": "User",
+            "email": "testuser@site.com",
+            "password": "1234567",
+        }
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("email", response.data)
+
+
+class LoginViewTest(APITestCase):
+    """
+    Test cases for Token authorization retrieve
+
+    Returns:
+        - 200 Ok: with a serialized representation that includes token, expiry, user
+        - 400 Bad request: when ValidationError thrown or user could not be found in db
+    """
+
+    def setUp(self):
+        self.url = reverse("login")
+        self.user = CustomUser.objects.create_user(
+            email="testuser@site.com",
+            password="testpass",
+            first_name="Testing",
+            last_name="DRF",
+        )
+
+    def test_can_login_valid_data(self):
+        data = {"username": "testuser@site.com", "password": "testpass"}
+
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+        self.assertIn("token", response.data)
+        self.assertIn("user", response.data)
+        self.assertIn("expiry", response.data)
+
+    def test_can_not_login_invalid_data(self):
+        # no matching data
+        invalid_data = {"username": "testuser@site.com", "password": "testPASS"}
+
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # no data
+        invalid_data = {}
+
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # missing email
+        invalid_data = {"password": "testPASS"}
+
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # missing password
+        invalid_data = {"username": "testuser@site.com"}
+
+        response = self.client.post(self.url, invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UserRetrieveViewTest(APITestCase):
+    """
+    Test cases for User model Retrieve view
+
+    Returns:
+        - 200 Ok: with a serialized representation of the user object
+        - 401 Unauthorized: When user didn't provide valid authorization
+    """
+
+    def setUp(self):
+        self.url = reverse("profile")
+        self.user = CustomUser.objects.create_user(
+            email="testuser@site.com",
+            password="testpass",
+            first_name="Testing",
+            last_name="DRF",
+        )
+        self.token = self.client.post(
+            reverse("login"),
+            {"username": "testuser@site.com", "password": "testpass"},
+            format="json",
+        ).data["token"]
+
+    def test_can_retrieve_information(self):
+        response = self.client.get(
+            self.url,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["first_name"], self.user.first_name)
+        self.assertEqual(response.data["last_name"], self.user.last_name)
+        self.assertEqual(response.data["email"], self.user.email)
+        self.assertEqual(response.data["title"], "")
+        self.assertEqual(response.data["description"], "")
+        self.assertEqual(response.data["name"], self.user.get_full_name())
+        self.assertEqual(len(response.data), 12)
+        self.assertIn("id", response.data)
+        self.assertIn("first_name", response.data)
+        self.assertIn("last_name", response.data)
+        self.assertIn("email", response.data)
+        self.assertIn("avatar", response.data)
+        self.assertIn("banner", response.data)
+        self.assertIn("title", response.data)
+        self.assertIn("description", response.data)
+        self.assertIn("name", response.data)
+        self.assertIn("followers", response.data)
+        self.assertIn("following", response.data)
+        self.assertIn("posts", response.data)
+
+    def test_unauthorized_retrieve_information(self):
+        response = self.client.get(self.url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PostListCreateViewTest(APITestCase):
+    """
+    Test cases for Post model List/Create view
+
+    Returns:
+        - 200 Ok: with a serialized representation of the queryset
+        - 201 Created: with a serialized representation of the post model object
+        - 400 Bad request: when raised ValidationError
+        - 401 Unauthorized: when failed to provided valid authentication
+    """
+
+    def setUp(self):
+        self.url = reverse("post-list-create")
+        self.user = CustomUser.objects.create_user(
+            email="testuser@site.com",
+            password="testpass",
+            first_name="Testing",
+            last_name="DRF",
+        )
+        self.token = self.client.post(
+            reverse("login"),
+            {"username": "testuser@site.com", "password": "testpass"},
+            format="json",
+        ).data["token"]
+
+    def test_can_get_list_posts(self):
+        # Create some posts in order to list them
+        Post.objects.create(user=self.user, title="Test", content="Test content")
+        Post.objects.create(user=self.user, title="Test 1", content="Test content")
+        Post.objects.create(user=self.user, title="Test 2", content="Test content")
+        Post.objects.create(user=self.user, title="Test 3", content="Test content")
+
+        response = self.client.get(
+            self.url,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 4)
+
+    def test_can_create_post_valid_data(self):
+        data = {"title": "Testing via DRF", "content": "Creation via DRF"}
+
+        response = self.client.post(
+            self.url,
+            data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 5)
+        self.assertIn("user", response.data)
+        self.assertIn("title", response.data)
+        self.assertIn("content", response.data)
+        self.assertIn("image", response.data)
+        self.assertIn("video", response.data)
+
+    def test_can_not_create_post_invalid_data(self):
+        # no data
+        invalid_data = {}
+
+        response = self.client.post(
+            self.url,
+            invalid_data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 2)
+        self.assertIn("title", response.data)
+        self.assertIn("content", response.data)
+
+        # no title
+        invalid_data = {"content": "This is a content"}
+
+        response = self.client.post(
+            self.url,
+            invalid_data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("title", response.data)
+
+        # no content
+        invalid_data = {"title": "This is a title"}
+
+        response = self.client.post(
+            self.url,
+            invalid_data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("content", response.data)
+
+    def test_unauthorized_access(self):
+        response = self.client.get(self.url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        data = {"title": "Testing via DRF", "content": "Creation via DRF"}
+        response = self.client.post(
+            self.url,
+            data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PostLikeCreateViewTest(APITestCase):
+    """
+    Test cases for PostLike model CreateView
+
+    Returns:
+        - 201 Created: with a serialized representation of the PostLike model object
+        - 400 Bad Request: when raised ValidationError, IntegrityError
+        - 401 Unauthorized: when failed to provide valid authorization
+    """
+
+    def setUp(self):
+        self.url = reverse("post-like-create")
+        self.user = CustomUser.objects.create_user(
+            email="testuser@site.com",
+            password="testpass",
+            first_name="Testing",
+            last_name="DRF",
+        )
+        self.token = self.client.post(
+            reverse("login"),
+            {"username": "testuser@site.com", "password": "testpass"},
+            format="json",
+        ).data["token"]
+        self.post = Post.objects.create(
+            user=self.user, title="Post test", content="This is a content"
+        )
+
+    def test_can_create_like_valid_data(self):
+        data = {"post": self.post.id}
+        response = self.client.post(
+            self.url, data, format="json", HTTP_AUTHORIZATION=f"Token {self.token}"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 2)
+        self.assertIn("user", response.data)
+        self.assertIn("post", response.data)
+
+    def test_can_not_create_like_invalid_data(self):
+        # no data
+        invalid_data = {}
+        response = self.client.post(
+            self.url,
+            invalid_data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("post", response.data)
+
+        # wrong id
+        invalid_data = {"post": "kadosa1"}
+        response = self.client.post(
+            self.url,
+            invalid_data,
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn("post", response.data)
+
+        # Can't like it twice
+        PostLike.objects.create(post=self.post, user=self.user)
+        data = {"post": self.post.id}
+        with self.assertRaises(Exception) as thrown_error:
+            response = self.client.post(
+                self.url, data, format="json", HTTP_AUTHORIZATION=f"Token {self.token}"
+            )
+        self.assertIsInstance(thrown_error.exception, IntegrityError)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_unauthorized_access(self):
+        data = {"post": self.post.id}
+
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PostLikeDestroyViewTest(APITestCase):
+    """
+    Test cases for PostLike model instances Destroy view
+
+    Returns:
+        - 204 No Content: if an object was deleted
+        - 401 Unathorized: when failed to provided valid authentication
+        - 404 Not Found: object to be deleted was not found or was not liked by user (based on queryset)
+    """
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="testuser@site.com",
+            password="testpass",
+            first_name="Testing",
+            last_name="DRF",
+        )
+        self.another_user = CustomUser.objects.create_user(
+            email="testuser1@site.com",
+            password="testpass",
+            first_name="Testing",
+            last_name="DRF",
+        )
+        self.token = self.client.post(
+            reverse("login"),
+            {"username": "testuser@site.com", "password": "testpass"},
+            format="json",
+        ).data["token"]
+        self.post = Post.objects.create(
+            user=self.user, title="Post test", content="This is a content"
+        )
+
+    def test_can_destroy_like_valid_action(self):
+        PostLike.objects.create(post=self.post, user=self.user)
+
+        response = self.client.delete(
+            reverse("post-like-destroy", kwargs={"post": self.post.id}),
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_can_not_destroy_list_invalid_action(self):
+        PostLike.objects.create(post=self.post, user=self.user)
+
+        # not found
+        response = self.client.delete(
+            reverse("post-like-destroy", kwargs={"post": "13165asd"}),
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # not owner
+        another_post = PostLike.objects.create(post=self.post, user=self.another_user)
+        response = self.client.delete(
+            reverse("post-like-destroy", kwargs={"post": another_post.id}),
+            format="json",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_unauthorized_access(self):
+        response = self.client.delete(
+            reverse("post-like-destroy", kwargs={"post": self.post.id}),
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
---
+++ backend/feed/tests/test_models.py
@@ -0,0 +1,201 @@
+from django.test import TestCase
+from accounts.models import CustomUser
+from feed.models import Post, Comment, PostLike, CommentLike
+from django.db import IntegrityError
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create an user for FK relationship
+        cls.user = CustomUser.objects.create_user(
+            email="test@user.com",
+            first_name="Testing",
+            last_name="Django",
+            password="testpassword",
+        )
+
+        # Create a test Post
+        cls.post = Post.objects.create(
+            user=cls.user, title="Test post", content="This is just content for testing"
+        )
+
+    def test_post_model_fields(self):
+        # Test the fields of the Post model
+        self.assertEqual(self.post.user, self.user)
+        self.assertEqual(self.post.title, "Test post")
+        self.assertEqual(self.post.content, "This is just content for testing")
+        self.assertEqual(self.post.image, None)
+        self.assertEqual(self.post.video, None)
+
+    def test_post_model_date_created(self):
+        # Test the date_created field is auto-generated correctly
+        self.assertIsNotNone(self.post.date_created)
+
+    def test_post_model_str_representation(self):
+        # Test the string representation of the Post model
+        expected_str = (
+            f"{self.user} posted: {self.post.title} - {self.post.date_created}"
+        )
+        self.assertEqual(str(self.post), expected_str)
+
+    # post likes
+    def test_post_model_likes_count(self):
+        self.assertEqual(self.post.likes.count(), 0)
+
+    # post comments
+    def test_post_model_comments_count(self):
+        self.assertEqual(self.post.comments.count(), 0)
+
+
+class CommentModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Creating model instances for FK relationships
+        # user
+        cls.user = CustomUser.objects.create_user(
+            email="normal@user.com",
+            password="textpassword",
+            first_name="Testing",
+            last_name="Django",
+        )
+
+        # post
+        cls.post = Post.objects.create(
+            user=cls.user, title="Post title", content="This is just a test"
+        )
+
+        # Comment
+        cls.comment = Comment.objects.create(
+            post=cls.post, user=cls.user, content="First ever comment"
+        )
+
+    def test_comment_model_fields(self):
+        self.assertEqual(self.comment.post, self.post)
+        self.assertEqual(self.comment.user, self.user)
+        self.assertEqual(self.comment.content, "First ever comment")
+        self.assertEqual(self.comment.parent, None)
+
+    def test_comment_model_date_created(self):
+        self.assertIsNotNone(self.comment.date_created)
+
+    def test_comment_model_str_representation(self):
+        expected_str = f"{self.user} comment on: {self.post}"
+        self.assertEqual(str(self.comment), expected_str)
+
+    def test_comment_model_parent_relationship(self):
+        parent_comment = Comment.objects.create(
+            post=self.post,
+            user=self.user,
+            content="This is a parent comment.",
+        )
+        reply_comment = Comment.objects.create(
+            post=self.post,
+            user=self.user,
+            content="This is a reply comment.",
+            parent=parent_comment,
+        )
+
+        # Check that the reply comment has the correct parent comment
+        self.assertEqual(reply_comment.parent, parent_comment)
+
+        # Check that the parent comment has the correct number of replies
+        self.assertEqual(parent_comment.replies.count(), 1)
+        self.assertEqual(parent_comment.replies.first(), reply_comment)
+
+    def test_comment_model_ordering(self):
+        # Test the ordering of the Comment model by date_created (descending)
+        comments_ordered = Comment.objects.all().order_by("-date_created")
+        self.assertQuerysetEqual(comments_ordered, Comment.objects.all(), ordered=True)
+
+    def test_comment_model_likes_count(self):
+        # Test initial count of likes
+        self.assertEqual(self.comment.likes.count(), 0)
+
+
+class PostLikeModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create those FK relationships needed
+        # user
+        cls.user = CustomUser.objects.create_user(
+            first_name="Testing",
+            last_name="User",
+            email="user@site.com",
+            password="textpassword",
+        )
+
+        # post
+        cls.post = Post.objects.create(
+            user=cls.user, title="Test post", content="Test content"
+        )
+
+        # Like instance
+        cls.post_like = PostLike.objects.create(post=cls.post, user=cls.user)
+
+    def test_post_like_model_fields(self):
+        self.assertEqual(self.post_like.post, self.post)
+        self.assertEqual(self.post_like.user, self.user)
+
+    # date
+    def test_post_like_model_date_like(self):
+        self.assertIsNotNone(self.post_like.date_like)
+
+    # str method
+    def test_post_like_model_str_representation(self):
+        expected_str = f"{self.user} liked: {self.post}"
+        self.assertEqual(str(self.post_like), expected_str)
+
+    # constraint
+    def test_post_like_model_unique_constraint(self):
+        # Attempt to create a duplicate like entry for the same post and user combination
+        with self.assertRaises(Exception) as error_thrown:
+            PostLike.objects.create(post=self.post, user=self.user)
+
+        # Check if an IntegrityError was raised
+        self.assertIsInstance(error_thrown.exception, IntegrityError)
+
+
+class CommentLikeModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Make sure all FK relationships are available
+        cls.user = CustomUser.objects.create_user(
+            email="normal@user.com",
+            password="textpassword",
+            first_name="Test",
+            last_name="Django",
+        )
+
+        cls.post = Post.objects.create(
+            user=cls.user, title="Test post", content="Test content"
+        )
+
+        cls.comment = Comment.objects.create(
+            post=cls.post, user=cls.user, content="Test content"
+        )
+
+        cls.comment_like = CommentLike.objects.create(
+            comment=cls.comment, user=cls.user
+        )
+
+    def test_comment_like_model_fields(self):
+        # Make sure we test all fields
+        self.assertEqual(self.comment_like.comment, self.comment)
+        self.assertEqual(self.comment_like.user, self.user)
+
+    def test_comment_like_model_date_added(self):
+        # Make sure date is getting added automatically
+        self.assertIsNotNone(self.comment_like.date_like)
+
+    def test_comment_like_model_str_representation(self):
+        # Make sure we getting the expected str
+        expected_str = f"{self.user} liked comment: {self.comment}"
+        self.assertEqual(str(self.comment_like), expected_str)
+
+    def test_comment_like_model_unique_constraint(self):
+        # Test for the uniqueness of the one like per user
+        with self.assertRaises(Exception) as error_thrown:
+            CommentLike.objects.create(comment=self.comment, user=self.user)
+
+        self.assertIsInstance(error_thrown.exception, IntegrityError)
