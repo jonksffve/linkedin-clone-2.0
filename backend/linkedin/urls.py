fc84df17d7b881a9503f83e3ba63122d579d0f01
Jonathan Ramirez <jonksff@gmail.com>
2023-07-22 11:53:10 -0400

    ADD: skip check coverage

--- backend/linkedin/urls.py
+++ backend/linkedin/urls.py
@@ -22,5 +22,9 @@ from django.urls import path, include
 urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls"))]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )  # pragma: no cover
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )  # pragma: no cover
