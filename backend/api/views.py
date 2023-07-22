dcc9d3a6c7bfee2eb6a6722b921efb61576c8b38
Jonathan Ramirez <jonksff@gmail.com>
2023-07-22 12:00:35 -0400

    ADD: new views, models

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,22 +3,32 @@ from django.contrib.auth import login
 from rest_framework import generics, permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 
-from accounts.models import CustomUser
-from accounts.serializers import UserCreationSerializer, UserSerializer
+from feed.models import Post, PostLike, Comment, CommentLike
+from feed.serializers import (
+    PostCreationSerializer,
+    PostListSerializer,
+    PostLikeSerializer,
+    CommentCreationSerializer,
+    CommentListSerializer,
+    CommentLikeSerializer,
+)
+from accounts.serializers import UserCreationSerializer, UserListSerializer
 
 from knox.views import LoginView as KnoxLoginView
 
 
+# Authentication
 class UserCreationView(generics.CreateAPIView):
     """
     Function that creates User model instances
 
     Returns:
-        - 200 Ok: with a serialized representation of the user object
-        - 400 Bad request: if ValidationError was raised
+        - 201 Created: with a serialized representation of the user object
+        - 400 Bad request: if ValidationError was raised or IntegrityError
 
     Raises:
         - ValidationError: when failed to validate data
+        - IntegrityError: when trying to use registered email
     """
 
     serializer_class = UserCreationSerializer
@@ -57,7 +67,100 @@ class UserRetrieveInformationView(generics.RetrieveAPIView):
     """
 
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = UserSerializer
+    serializer_class = UserListSerializer
 
     def get_object(self):
         return self.request.user
+
+
+# Feed
+class PostListCreateView(generics.ListCreateAPIView):
+    """
+    View that either CREATES or LIST Post model instances
+
+    Returns:
+        - 200 Ok: with a serialized representation of the queryset
+        - 201 Created: with a serialized representation of the post model object
+        - 400 Bad request: when raised ValidationError
+        - 401 Unauthorized: when failed to provided valid authentication
+
+    Raises:
+        - ValidationError: when invalid request.data was provided
+
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = None
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return PostCreationSerializer
+        return PostListSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostLikeCreateView(generics.CreateAPIView):
+    """
+    View that creates a PostLike model instance
+
+    Returns:
+        - 201 Created: with a serialized representation of the PostLike model object
+        - 400 Bad Request: when raised ValidationError, IntegrityError
+        - 401 Unauthorized: when failed to provide valid authorization
+
+    Raises:
+        - ValidationError: when request.data provided was invalid
+        - IntegrityError: when trying to like twice
+
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PostLikeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostLikeDestroyView(generics.DestroyAPIView):
+    """
+    View that perfoms the destruction of a PostLike model instance
+
+    Returns:
+        - 204 No Content: if an object was deleted
+        - 401 Unathorized: when failed to provided valid authentication
+        - 404 Not Found: object to be deleted was not found or was not liked by user (based on queryset)
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = None
+    serializer_class = PostLikeSerializer
+    lookup_field = "post"
+
+    def get_queryset(self):
+        return PostLike.objects.filter(user=self.request.user)
+
+
+# class CommentListCreateView(generics.ListCreateAPIView):
+#     permission_classes = [permissions.IsAuthenticated]
+#     queryset = None
+#     serializer_class = None
+
+#     def get_serializer_class(self):
+#         if self.request.method == "POST":
+#             return CommentCreationSerializer
+#         return CommentListSerializer
+
+#     def get_queryset(self):
+#         if self.request.method == "GET":
+#             return Comment
+
+
+# class CommentLikeCreateView:
+#     pass
+
+
+# class CommentLikeDestroyView:
+#     pass
--- backend/feed/models.py
+++ backend/feed/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(
+        "accounts.Customuser",
+        verbose_name=_("creator"),
+        related_name="posts",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(_("title"), max_length=50)
+    content = models.TextField(_("content"))
+    date_created = models.DateTimeField(
+        _("date created"), auto_now=False, auto_now_add=True
+    )
+    image = models.ImageField(
+        _("image"),
+        upload_to="post/image/",
+        blank=True,
+        null=True,
+    )
+    video = models.FileField(_("video"), upload_to="post/video/", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} posted: {self.title} - {self.date_created}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        verbose_name=_("Comment"),
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        "accounts.CustomUser", verbose_name=_("Comment user"), on_delete=models.CASCADE
+    )
+    content = models.TextField(_("Comment content"))
+    date_created = models.DateTimeField(_("Comment date"), auto_now_add=True)
+    parent = models.ForeignKey(
+        "self",
+        verbose_name=_("Comment reply"),
+        related_name="replies",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        ordering = ["-date_created"]
+
+    def __str__(self):
+        return f"{self.user} comment on: {self.post}"
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(
+        Post,
+        verbose_name=_("Post likes"),
+        related_name="likes",
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        "accounts.CustomUser", verbose_name=_("User"), on_delete=models.CASCADE
+    )
+    date_like = models.DateTimeField(_("Like date"), auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["post", "user"], name="unique_like_post")
+        ]
+
+    def __str__(self):
+        return f"{self.user} liked: {self.post}"
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(
+        Comment,
+        verbose_name=_("Comment likes"),
+        related_name="likes",
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        "accounts.CustomUser", verbose_name=_("User"), on_delete=models.CASCADE
+    )
+    date_like = models.DateTimeField(_("Like date"), auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["comment", "user"], name="unique_like_comment"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} liked comment: {self.comment}"
