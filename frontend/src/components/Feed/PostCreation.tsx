69a5d52da2650c71d07610c9ec5d4f61994c7bba
Jonathan Ramirez <jonksff@gmail.com>
2023-07-25 19:54:03 -0400

    ADD: handleOpenModal logic

--- frontend/src/components/Feed/PostCreation.tsx
+++ frontend/src/components/Feed/PostCreation.tsx
@@ -2,9 +2,17 @@ import { useAppSelector } from '@/store/hooks';
 import Button from '../HTMLelements/Buttons/Button';
 import { BsCalendarEvent, BsFileImage } from 'react-icons/bs';
 import { TfiCommentAlt } from 'react-icons/tfi';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '@/store/slices/ui-slice';
 
 const PostCreation = () => {
 	const userState = useAppSelector((state) => state.user);
+	const dispatch = useDispatch();
+
+	const handleOpenModal = useCallback(() => {
+		dispatch(uiActions.onShowUploadModal());
+	}, [dispatch]);
 
 	return (
 		<div className='bg-white rounded-sm p-2 flex flex-col gap-2'>
@@ -14,7 +22,10 @@ const PostCreation = () => {
 					src={userState.avatar}
 					alt=''
 				/>
-				<Button className='w-full hover:bg-neutral-200 bg-blue-50'>
+				<Button
+					className='w-full hover:bg-neutral-200 bg-blue-50'
+					onClick={handleOpenModal}
+				>
 					<span className='absolute left-5 text-neutral-400'>
 						{userState.name}, what are you thinking?
 					</span>
@@ -22,11 +33,17 @@ const PostCreation = () => {
 			</div>
 			<hr />
 			<div className='w-full flex flex-row'>
-				<Button className='w-full hover:bg-neutral-100 text-neutral-400 rounded-sm font-semibold font-sans'>
+				<Button
+					className='w-full hover:bg-neutral-100 text-neutral-400 rounded-sm font-semibold font-sans'
+					onClick={handleOpenModal}
+				>
 					Foto/Video
 					<BsFileImage className='absolute left-5' />
 				</Button>
-				<Button className='w-full hover:bg-neutral-100 text-neutral-400 rounded-sm font-semibold font-sans'>
+				<Button
+					className='w-full hover:bg-neutral-100 text-neutral-400 rounded-sm font-semibold font-sans'
+					onClick={handleOpenModal}
+				>
 					Post
 					<TfiCommentAlt className='absolute left-5' />
 				</Button>
