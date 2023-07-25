b990b450625a9c97f415c7a85c1210311cd72ff4
Jonathan Ramirez <jonksff@gmail.com>
2023-07-25 13:59:10 -0400

    ADD: tests

---
+++ backend/accounts/tests/test_serializers.py
@@ -0,0 +1,89 @@
+from django.test import TestCase
+from ..serializers import UserCreationSerializer, UserListSerializer
+from ..models import CustomUser
+
+
+class TestUserCreationSerializer(TestCase):
+    def test_deserialization_valid_data(self):
+        valid_data = {
+            "first_name": "Jonathan",
+            "last_name": "Ramirez",
+            "email": "test@site.com",
+            "password": "testpassword",
+        }
+
+        serializer = UserCreationSerializer(data=valid_data)
+
+        self.assertTrue(serializer.is_valid())
+
+        user_instance = serializer.save()
+
+        self.assertIsInstance(user_instance, CustomUser)
+        self.assertEqual(user_instance.username, None)
+        self.assertEqual(user_instance.first_name, "Jonathan")
+        self.assertEqual(user_instance.last_name, "Ramirez")
+        self.assertEqual(user_instance.email, "test@site.com")
+        self.assertIsNotNone(user_instance.avatar, None)
+        self.assertIsNotNone(user_instance.banner, None)
+        self.assertEqual(user_instance.title, "")
+        self.assertEqual(user_instance.description, "")
+        # test hashing password
+        self.assertNotEqual(user_instance.password, valid_data["password"])
+
+    def test_deserialization_invalid_data(self):
+        invalid_data = [
+            # no data
+            {},
+            # no first_name
+            {"last_name": "User", "email": "user@site.com", "password": "1234test"},
+            # no last_name
+            {"first_name": "Testing", "email": "user@site.com", "password": "1234test"},
+            # no email
+            {"first_name": "Testing", "last_name": "User", "password": "1234test"},
+            # no password
+            {"first_name": "Testing", "last_name": "User", "email": "user@site.com"},
+        ]
+
+        for data in invalid_data:
+            serializer = UserCreationSerializer(data=data)
+            self.assertFalse(serializer.is_valid())
+
+
+class TestUserListSerializer(TestCase):
+    def test_serialization(self):
+        raw_user_instance = CustomUser.objects.create_user(
+            email="site@user.com",
+            password="123test",
+            first_name="Test",
+            last_name="User",
+        )
+
+        serializer = UserListSerializer(instance=raw_user_instance)
+
+        # serialized data
+        serialized_data = serializer.data
+
+        # all the fields
+        self.assertTrue(len(serialized_data), 12)
+        self.assertIn("id", serialized_data)
+        self.assertIn("first_name", serialized_data)
+        self.assertIn("last_name", serialized_data)
+        self.assertIn("email", serialized_data)
+        self.assertIn("avatar", serialized_data)
+        self.assertIn("banner", serialized_data)
+        self.assertIn("title", serialized_data)
+        self.assertIn("description", serialized_data)
+        self.assertIn("name", serialized_data)
+        self.assertIn("followers", serialized_data)
+        self.assertIn("following", serialized_data)
+        self.assertIn("posts", serialized_data)
+        self.assertNotIn("password", serialized_data)
+
+        # get_name works
+        self.assertEqual(serialized_data["name"], "Test User")
+
+        # field values
+        self.assertEqual(serialized_data["email"], raw_user_instance.email)
+        self.assertEqual(len(serialized_data["followers"]), 0)
+        self.assertEqual(len(serialized_data["following"]), 0)
+        self.assertEqual(len(serialized_data["posts"]), 0)
---
+++ backend/feed/tests/test_serializers.py
@@ -0,0 +1,521 @@
+from django.test import TestCase
+
+from accounts.models import CustomUser
+from accounts.serializers import UserListSerializer
+
+from ..models import Post, Comment, PostLike, CommentLike
+from ..serializers import (
+    PostSerializer,
+    CommentSerializer,
+    PostLikeSerializer,
+    CommentLikeSerializer,
+)
+
+
+class TestPostSerializer(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="test@user.com",
+            password="testpassword",
+            first_name="hello",
+            last_name="goodbye",
+        )
+
+        self.raw_post = Post.objects.create(
+            user=self.user, title="Raw object", content="Raw object's content"
+        )
+
+        self.expected_valid_data = {
+            "title": "Post test",
+            "content": "Post test content",
+        }
+
+    def test_post_serialization(self):
+        serializer = PostSerializer(instance=self.raw_post)
+
+        # serialization
+        serialized_data = serializer.data
+
+        # check fields in the serializer
+        self.assertEqual(len(serialized_data), 7)
+        self.assertEqual(serialized_data["id"], self.raw_post.id)
+        self.assertEqual(serialized_data["user"]["id"], self.raw_post.user.id)
+        self.assertEqual(serialized_data["title"], self.raw_post.title)
+        self.assertEqual(serialized_data["content"], self.raw_post.content)
+        self.assertIsNotNone(serialized_data["date_created"])
+
+        # image, video read_only should not be included (if not given)
+        self.assertIsNone(serialized_data["video"])
+        self.assertIsNone(serialized_data["image"])
+
+    def test_post_serialization_multiple(self):
+        # Create multiple test posts
+        Post.objects.create(
+            title="Second Test Post",
+            content="This is another test post.",
+            user=self.user,
+        )
+
+        Post.objects.create(
+            title="Third Test Post",
+            content="This is a third test post.",
+            user=self.user,
+        )
+
+        queryset = Post.objects.all()
+        serializer = PostSerializer(instance=queryset, many=True)
+
+        # Check the serialized data for multiple instances
+        self.assertEqual(len(serializer.data), 3)  # Three posts serialized
+
+    def test_post_serialization_user_serialization(self):
+        serializer = PostSerializer(instance=self.raw_post)
+
+        # serialization
+        serialized_data = serializer.data
+
+        user_serializer = UserListSerializer(instance=self.user)
+
+        self.assertEqual(serialized_data["user"], user_serializer.data)
+
+    def test_post_deserialization_valid_data(self):
+        serializer = PostSerializer(data=self.expected_valid_data)
+
+        self.assertTrue(serializer.is_valid())
+
+        post_instance = serializer.save(user=self.user)
+
+        self.assertIsInstance(post_instance, Post)
+        self.assertEqual(post_instance.title, "Post test")
+        self.assertEqual(post_instance.content, "Post test content")
+        self.assertEqual(post_instance.user.id, self.user.id)
+        self.assertIsNotNone(post_instance.date_created)
+        self.assertEqual(post_instance.image, None)
+        self.assertEqual(post_instance.video, None)
+
+    def test_post_deserialization_invalid_data(self):
+        # no data
+        invalid_data = {}
+        serializer = PostSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 2)
+        self.assertIn("title", serializer.errors)
+        self.assertIn("content", serializer.errors)
+
+        # no title
+        invalid_data = {"title": "", "content": "This is a content"}
+        serializer = PostSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("title", serializer.errors)
+
+        # no content
+        invalid_data = {"title": "Title"}
+        serializer = PostSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("content", serializer.errors)
+
+
+class TestCommentSerializer(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="user@site.com", password="testpass"
+        )
+
+        self.post = Post.objects.create(
+            user=self.user, title="This is a post", content="This a content"
+        )
+
+        self.raw_comment = Comment.objects.create(
+            post=self.post, user=self.user, content="This a comment content"
+        )
+
+        self.expected_data_comment = {
+            "post": self.post.id,
+            "content": "This is a comment.",
+        }
+
+        self.expected_data_reply = {
+            "post": self.post.id,
+            "content": "This is a reply",
+            "parent": self.raw_comment.id,
+        }
+
+    def test_comment_deserialization_valid_data(self):
+        serializer = CommentSerializer(data=self.expected_data_comment)
+
+        self.assertTrue(serializer.is_valid())
+
+        # get the model instance
+        comment_instance = serializer.save(user=self.user)
+
+        self.assertIsInstance(comment_instance, Comment)
+        # Checking fields of the deserialized representation
+        self.assertEqual(comment_instance.post, self.post)
+        self.assertEqual(comment_instance.user, self.user)
+        self.assertEqual(
+            comment_instance.content, self.expected_data_comment["content"]
+        )
+        self.assertIsNotNone(comment_instance.date_created)
+        self.assertIsNone(comment_instance.parent)
+
+    def test_comment_deserialization_invalid_data_cases(self):
+        # no data
+        invalid_data = {}
+
+        serializer = CommentSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 2)
+        self.assertIn("post", serializer.errors)
+        self.assertIn("content", serializer.errors)
+
+        # no post
+        invalid_data = {"content": "this is a content"}
+
+        serializer = CommentSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("post", serializer.errors)
+
+        # no content
+        invalid_data = {"post": self.post.id}
+
+        serializer = CommentSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("content", serializer.errors)
+
+    def test_reply_deserialization_valid_data(self):
+        serializer = CommentSerializer(data=self.expected_data_reply)
+
+        self.assertTrue(serializer.is_valid())
+
+        comment_with_reply_instance = serializer.save(user=self.user)
+
+        self.assertIsInstance(comment_with_reply_instance, Comment)
+        self.assertEqual(comment_with_reply_instance.post, self.post)
+        self.assertEqual(comment_with_reply_instance.user, self.user)
+        self.assertEqual(
+            comment_with_reply_instance.content, self.expected_data_reply["content"]
+        )
+        self.assertIsNotNone(comment_with_reply_instance.date_created)
+        self.assertEqual(comment_with_reply_instance.parent, self.raw_comment)
+
+    def test_reply_deserialization_invalid_data_case(self):
+        # invalid post
+        invalid_data = {
+            "post": 123,
+            "content": "this is a reply",
+            "parent": self.raw_comment.id,
+        }
+
+        serializer = CommentSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+
+        self.assertEqual(len(serializer.errors), 2)
+        self.assertIn("parent", serializer.errors)
+        self.assertIn("post", serializer.errors)
+
+        # invalid parent
+        invalid_data = {
+            "post": self.post.id,
+            "content": "I'm a valid content",
+            "parent": 123,
+        }
+
+        serializer = CommentSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("parent", serializer.errors)
+
+        # TEST: validate_parent (valid: post, valid: parent. Not from the same post tho.)
+        another_post = Post.objects.create(
+            user=self.user, title="Another post", content="Another content"
+        )
+
+        invalid_data = {
+            "post": another_post.id,
+            "content": "I'm a valid content",
+            "parent": self.raw_comment,
+        }
+
+        serializer = CommentSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("parent", serializer.errors)
+
+    def test_comment_serialization(self):
+        serializer = CommentSerializer(instance=self.raw_comment)
+
+        # serialized data
+        serialized_data = serializer.data
+
+        self.assertEqual(len(serialized_data), 7)
+        self.assertIn("id", serialized_data)
+        self.assertEqual(serialized_data["post"], self.raw_comment.post.id)
+        self.assertEqual(serialized_data["user"]["id"], self.raw_comment.user.id)
+        self.assertEqual(serialized_data["content"], self.raw_comment.content)
+        self.assertIsNotNone(serialized_data["date_created"])
+        self.assertIsNone(serialized_data["parent"])
+        self.assertEqual(serialized_data["replies_count"], 0)
+
+    def test_reply_serialization(self):
+        reply_instance = Comment.objects.create(
+            post=self.post,
+            user=self.user,
+            content="Im a reply",
+            parent=self.raw_comment,
+        )
+
+        serializer = CommentSerializer(instance=reply_instance)
+
+        # serialized data
+        serialized_data = serializer.data
+
+        self.assertEqual(len(serialized_data), 7)
+        self.assertIn("id", serialized_data)
+        self.assertEqual(serialized_data["post"], reply_instance.post.id)
+        self.assertEqual(serialized_data["user"]["id"], reply_instance.user.id)
+        self.assertEqual(serialized_data["content"], reply_instance.content)
+        self.assertIsNotNone(serialized_data["date_created"])
+        self.assertEqual(serialized_data["parent"], reply_instance.parent.id)
+        self.assertEqual(serialized_data["replies_count"], 0)
+
+    def test_comment_serialization_user_serializer(self):
+        serializer = CommentSerializer(instance=self.raw_comment)
+
+        # serialized data
+        serialized_data = serializer.data
+
+        # serialized user
+        serialized_user = UserListSerializer(instance=self.raw_comment.user)
+
+        self.assertEqual(serialized_data["user"], serialized_user.data)
+
+
+class TestPostLikeSerializer(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="user@site.com",
+            password="testpassword",
+            first_name="Testing",
+            last_name="User",
+        )
+        self.post = Post.objects.create(
+            user=self.user, title="This is a title", content="This is a content"
+        )
+
+        self.valid_data = {"post": self.post.id}
+
+    def test_deserialization_valid_data(self):
+        serializer = PostLikeSerializer(data=self.valid_data)
+
+        self.assertTrue(serializer.is_valid())
+
+        # deserialized instance
+        post_like_instance = serializer.save(user=self.user)
+
+        self.assertIsInstance(post_like_instance, PostLike)
+        self.assertEqual(post_like_instance.post, self.post)
+        self.assertEqual(post_like_instance.user, self.user)
+
+    def test_deserialization_invalid_data(self):
+        # no data
+        invalid_data = {}
+
+        serializer = PostLikeSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("post", serializer.errors)
+
+        # wrong post
+        invalid_data = {"post": 123}
+
+        serializer = PostLikeSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("post", serializer.errors)
+
+    def test_serialization_single(self):
+        raw_post_like = PostLike.objects.create(post=self.post, user=self.user)
+
+        serializer = PostLikeSerializer(instance=raw_post_like)
+
+        # getting serialized data
+        serialized_data = serializer.data
+
+        self.assertEqual(serialized_data["post"], raw_post_like.post.id)
+        self.assertEqual(serialized_data["user"]["id"], raw_post_like.user.id)
+
+    def test_serialization_multiple(self):
+        # Creating users
+        user1 = CustomUser.objects.create_user(
+            email="user1@site.com",
+            password="testpassword",
+            first_name="Testing",
+            last_name="Testing",
+        )
+        user2 = CustomUser.objects.create_user(
+            email="user2@site.com",
+            password="testpassword",
+            first_name="Testing",
+            last_name="Testing",
+        )
+        user3 = CustomUser.objects.create_user(
+            email="user3@site.com",
+            password="testpassword",
+            first_name="Testing",
+            last_name="Testing",
+        )
+
+        # Creating Likes
+        PostLike.objects.create(post=self.post, user=user1)
+        PostLike.objects.create(post=self.post, user=user2)
+        PostLike.objects.create(post=self.post, user=user3)
+
+        # Making the queryset
+        queryset = PostLike.objects.all()
+
+        # serialization of queryset
+        serializer = PostLikeSerializer(instance=queryset, many=True)
+
+        # data
+        serialized_data = serializer.data
+
+        self.assertEqual(len(serialized_data), 3)
+
+    def test_serialization_user_serialization(self):
+        raw_post_like = PostLike.objects.create(post=self.post, user=self.user)
+
+        serializer = PostLikeSerializer(instance=raw_post_like)
+
+        # getting serialized data
+        serialized_data = serializer.data
+
+        # user serializer
+        user_serializer = UserListSerializer(instance=raw_post_like.user)
+
+        self.assertEqual(serialized_data["user"], user_serializer.data)
+
+
+class TestCommentLikeSerializer(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            email="user@site.com",
+            password="testpassword",
+            first_name="User",
+            last_name="Object",
+        )
+        self.post = Post.objects.create(
+            user=self.user, title="This is a title", content="This is a content"
+        )
+        self.comment = Comment.objects.create(
+            post=self.post, user=self.user, content="This is a comment."
+        )
+
+    def test_deserialization_valid_data(self):
+        valid_data = {"comment": self.comment.id}
+        serializer = CommentLikeSerializer(data=valid_data)
+
+        self.assertTrue(serializer.is_valid())
+
+        comment_like_instance = serializer.save(user=self.user)
+
+        self.assertIsInstance(comment_like_instance, CommentLike)
+        self.assertEqual(comment_like_instance.comment, self.comment)
+        self.assertEqual(comment_like_instance.user, self.user)
+
+    def test_deserialization_invalid_data(self):
+        # no comment
+        invalid_data = {}
+        serializer = CommentLikeSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("comment", serializer.errors)
+
+        # wrong comment
+        invalid_data = {"comment": 123}
+        serializer = CommentLikeSerializer(data=invalid_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors), 1)
+        self.assertIn("comment", serializer.errors)
+
+    def test_serialization_single(self):
+        raw_model_instance = CommentLike.objects.create(
+            comment=self.comment, user=self.user
+        )
+
+        serializer = CommentLikeSerializer(instance=raw_model_instance)
+
+        serialized_data = serializer.data
+
+        self.assertEqual(serialized_data["comment"], self.comment.id)
+        self.assertEqual(serialized_data["user"]["id"], self.user.id)
+
+    def test_serialization_multiple(self):
+        # Creating users
+        user1 = CustomUser.objects.create_user(
+            email="user1@site.com",
+            password="testpassword",
+            first_name="Testing",
+            last_name="Testing",
+        )
+        user2 = CustomUser.objects.create_user(
+            email="user2@site.com",
+            password="testpassword",
+            first_name="Testing",
+            last_name="Testing",
+        )
+        user3 = CustomUser.objects.create_user(
+            email="user3@site.com",
+            password="testpassword",
+            first_name="Testing",
+            last_name="Testing",
+        )
+
+        # Creating Likes
+        CommentLike.objects.create(comment=self.comment, user=user1)
+        CommentLike.objects.create(comment=self.comment, user=user2)
+        CommentLike.objects.create(comment=self.comment, user=user3)
+
+        # Making the queryset
+        queryset = CommentLike.objects.all()
+
+        # serialization of queryset
+        serializer = CommentLikeSerializer(instance=queryset, many=True)
+
+        # data
+        serialized_data = serializer.data
+
+        self.assertEqual(len(serialized_data), 3)
+
+    def test_serialization_user_serialization(self):
+        raw_model_instance = CommentLike.objects.create(
+            comment=self.comment, user=self.user
+        )
+
+        serializer = CommentLikeSerializer(instance=raw_model_instance)
+
+        serialized_data = serializer.data
+
+        serialized_user = UserListSerializer(instance=self.user)
+
+        self.assertEqual(serialized_data["user"], serialized_user.data)
