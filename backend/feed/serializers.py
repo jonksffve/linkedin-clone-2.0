1d6948c71e4c1f4955b7f29448ce54362d55916e
Jonathan Ramirez <jonksff@gmail.com>
2023-07-25 13:24:35 -0400

    UPDATED: serializers

--- backend/feed/serializers.py
+++ backend/feed/serializers.py
@@ -3,34 +3,51 @@ from .models import Post, PostLike, Comment, CommentLike
 from accounts.serializers import UserListSerializer
 
 
-class PostCreationSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     user = UserListSerializer(read_only=True)
-
-    class Meta:
-        model = Post
-        fields = ["title", "content", "image", "video", "user"]
-
-
-class PostListSerializer(serializers.ModelSerializer):
-    user = UserListSerializer()
+    image = serializers.ImageField(allow_empty_file=True, required=False, default=None)
+    video = serializers.FileField(allow_empty_file=True, required=False, default=None)
 
     class Meta:
         model = Post
         fields = ["id", "user", "title", "content", "date_created", "image", "video"]
 
 
-class CommentCreationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ["post", "content", "parent"]
-
-
-class CommentListSerializer(serializers.ModelSerializer):
-    user = UserListSerializer()
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserListSerializer(read_only=True)
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=Comment.objects.all(), required=False, allow_null=True, default=None
+    )
+    replies_count = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ["id", "post", "user", "content", "date_created", "parent"]
+        fields = [
+            "id",
+            "post",
+            "user",
+            "content",
+            "date_created",
+            "parent",
+            "replies_count",
+        ]
+
+    def get_replies_count(self, obj):
+        return obj.replies.count()
+
+    # validation replies
+    def validate_parent(self, parent):
+        try:
+            post_id = self.initial_data["post"]
+        except:
+            post_id = None
+
+        if parent and post_id and parent.post.id != post_id:
+            raise serializers.ValidationError(
+                "Reply must be from the same post as the comment."
+            )
+
+        return parent
 
 
 class PostLikeSerializer(serializers.ModelSerializer):
@@ -42,9 +59,8 @@ class PostLikeSerializer(serializers.ModelSerializer):
 
 
 class CommentLikeSerializer(serializers.ModelSerializer):
-    user = UserListSerializer()
+    user = UserListSerializer(read_only=True)
 
     class Meta:
         model = CommentLike
         fields = ["comment", "user"]
-        read_only_fields = ["user"]
