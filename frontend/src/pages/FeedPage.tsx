a2e010674df56e6c738aad14bdadfe40a1b81234
Jonathan Ramirez <jonksff@gmail.com>
2023-07-27 13:03:18 -0400

    EDIT: styles

--- frontend/src/pages/FeedPage.tsx
+++ frontend/src/pages/FeedPage.tsx
@@ -1,7 +1,7 @@
 import { getPosts } from '@/api/feed';
 import ConnectionsSummary from '@/components/Feed/ConnectionsSummary';
-import PostContent from '@/components/Feed/PostContent';
-import PostCreation from '@/components/Feed/PostCreation';
+import PostContent from '@/components/Feed/Post/PostContent';
+import PostCreation from '@/components/Feed/Post/PostCreation';
 import ProfileSummary from '@/components/Feed/ProfileSummary';
 import UploadModal from '@/components/Modals/UploadModal';
 import Spinner from '@/components/Spinner';
@@ -23,10 +23,10 @@ const FeedPage = () => {
 	return (
 		<>
 			<div className='flex space-x-4'>
-				<div className='w-1/4 p-2 sticky top-0 left-0 h-screen'>
+				<div className='hidden md:block w-1/4 p-2 sticky top-0 left-0 h-screen'>
 					<ProfileSummary />
 				</div>
-				<div className='w-1/2 p-2 mx-auto'>
+				<div className='w-full md:w-1/2 p-2 mx-auto'>
 					<div className='p-2'>
 						<PostCreation />
 						{isLoading && <Spinner />}
@@ -44,7 +44,7 @@ const FeedPage = () => {
 							))}
 					</div>
 				</div>
-				<div className='w-1/4 p-2 sticky top-0 right-0 h-screen'>
+				<div className='hidden md:block w-1/4 p-2 sticky top-0 right-0 h-screen'>
 					<ConnectionsSummary />
 				</div>
 			</div>
