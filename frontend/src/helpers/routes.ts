54551a7643525c78ab15a58e2a13bc85957e4af0
jonksff <jonksff@gmail.com>
2023-08-03 11:36:04 -0400

    ADD: route profile

--- frontend/src/helpers/routes.ts
+++ frontend/src/helpers/routes.ts
@@ -3,6 +3,7 @@ export const ROUTE_INDEX = '/';
 export const ROUTE_CHAT = '/chat/';
 export const ROUTE_JOBS = '/jobs/';
 export const ROUTE_FEED = '/feed/';
+export const ROUTE_PROFILE = '/profile/';
 export const ROUTE_CONNECTIONS = '/connections/';
 export const ROUTE_NOTIFICATIONS = '/notifications/';
 
