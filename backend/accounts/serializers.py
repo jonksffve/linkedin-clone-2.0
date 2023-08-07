68cc43d2f1455346e5c984797bd3e1d21978959a
jonksff <jonksff@gmail.com>
2023-08-07 17:43:46 -0400

    ADD: new fields

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -42,20 +42,9 @@ class UserListSerializer(serializers.ModelSerializer):
             "banner",
             "title",
             "description",
-            "name",
-            "get_followers",
-            "get_following",
-            "get_posts",
-        ]
-        read_only_fields = [
-            "id",
-            "first_name",
-            "last_name",
-            "email",
-            "avatar",
-            "banner",
-            "title",
-            "description",
+            "university",
+            "actual_work",
+            "location",
             "name",
             "get_followers",
             "get_following",
@@ -63,17 +52,26 @@ class UserListSerializer(serializers.ModelSerializer):
         ]
 
 
-class UserUpdateSerializer(serializers.ModelSerializer):
+class UserInformationUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["first_name", "last_name", "avatar", "banner", "title", "description"]
+        fields = [
+            "first_name",
+            "last_name",
+            "title",
+            "description",
+            "university",
+            "actual_work",
+            "location",
+        ]
 
     def update(self, instance, validated_data):
         instance.first_name = validated_data.get("first_name", instance.first_name)
         instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.avatar = validated_data.get("avatar", instance.avatar)
-        instance.banner = validated_data.get("banner", instance.banner)
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get("description", instance.description)
+        instance.university = validated_data.get("university", instance.university)
+        instance.actual_work = validated_data.get("actual_work", instance.actual_work)
+        instance.location = validated_data.get("location", instance.location)
         instance.save()
         return instance
