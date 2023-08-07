78e4c78fb79869b10c2a7d10efc0947b8dccf3ec
jonksff <jonksff@gmail.com>
2023-08-07 14:25:47 -0400

    ADD: userUpdate api

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,113 +1,115 @@
 import { toast } from 'react-toastify';
 import { toastConfig } from '@/helpers/toastifyConfig';
-import {
-	ErrorResponse,
-	LoginFormInputs,
-	RegisterFormInputs,
-} from '@/helpers/types';
+import { ErrorResponse, LoginFormInputs, RegisterFormInputs, UserUpdateForm } from '@/helpers/types';
 import axios from 'axios';
 import {
-	ENDPOINT_ACCOUNT,
-	ENDPOINT_LOGIN,
-	ENDPOINT_LOGOUT,
-	ENDPOINT_PROFILE,
+    ENDPOINT_ACCOUNT,
+    ENDPOINT_LOGIN,
+    ENDPOINT_LOGOUT,
+    ENDPOINT_PROFILE,
+    ENDPOINT_PROFILE_EDIT,
 } from '@/helpers/routes';
 import { UseFormReset, UseFormSetError } from 'react-hook-form';
 
 export const createUserAPI = async (
-	data: RegisterFormInputs,
-	setIsLoading: (value: boolean) => void,
-	setError: UseFormSetError<RegisterFormInputs>,
-	reset: UseFormReset<RegisterFormInputs>,
-	showLoginHandler: () => void
+    data: RegisterFormInputs,
+    setIsLoading: (value: boolean) => void,
+    setError: UseFormSetError<RegisterFormInputs>,
+    reset: UseFormReset<RegisterFormInputs>,
+    showLoginHandler: () => void
 ) => {
-	try {
-		setIsLoading(true);
-		await axios.post(ENDPOINT_ACCOUNT, data, {
-			headers: {
-				'Content-Type': 'multipart/form-data',
-			},
-		});
-		reset();
-		showLoginHandler();
-		toast.success('Account created successfully', toastConfig);
-	} catch (error) {
-		if (axios.isAxiosError(error)) {
-			if (error.response) {
-				const data = error.response.data as ErrorResponse;
+    try {
+        setIsLoading(true);
+        await axios.post(ENDPOINT_ACCOUNT, data, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        reset();
+        showLoginHandler();
+        toast.success('Account created successfully', toastConfig);
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response) {
+                const data = error.response.data as ErrorResponse;
 
-				for (const field in data) {
-					setError(field, {
-						type: 'custom',
-						message: data[field as keyof ErrorResponse],
-					});
-				}
-			} else {
-				toast.error(
-					`An unexpected error ocurred: ${error.message}`,
-					toastConfig
-				);
-			}
-		}
-	} finally {
-		setIsLoading(false);
-	}
+                for (const field in data) {
+                    setError(field, {
+                        type: 'custom',
+                        message: data[field as keyof ErrorResponse],
+                    });
+                }
+            } else {
+                toast.error(`An unexpected error ocurred: ${error.message}`, toastConfig);
+            }
+        }
+    } finally {
+        setIsLoading(false);
+    }
 };
 
-export const createTokenAuthAPI = async (
-	data: LoginFormInputs,
-	setIsLoading: (value: boolean) => void
-) => {
-	try {
-		setIsLoading(true);
-		const response = await axios.post(ENDPOINT_LOGIN, data);
-		toast.success('Welcome back user', toastConfig);
-		return response;
-	} catch (error) {
-		if (axios.isAxiosError(error)) {
-			throw error;
-		} else {
-			toast.error('Something unexpected happened.', toastConfig);
-		}
-	} finally {
-		setIsLoading(false);
-	}
+export const createTokenAuthAPI = async (data: LoginFormInputs, setIsLoading: (value: boolean) => void) => {
+    try {
+        setIsLoading(true);
+        const response = await axios.post(ENDPOINT_LOGIN, data);
+        toast.success('Welcome back user', toastConfig);
+        return response;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw error;
+        } else {
+            toast.error('Something unexpected happened.', toastConfig);
+        }
+    } finally {
+        setIsLoading(false);
+    }
 };
 
 export const getUserInformationAPI = async (token: string, userEmail?: string) => {
-	try {
-		let url = ENDPOINT_PROFILE
+    try {
+        let url = ENDPOINT_PROFILE;
 
-		if (userEmail) {
-			url += `?email=${userEmail}`
-		}
+        if (userEmail) {
+            url += `?email=${userEmail}`;
+        }
 
-		const response = await axios.get(url, {
-			headers: {
-				Authorization: `Token ${token}`,
-			},
-		});
-		return response;
-	} catch (error) {
-		if (axios.isAxiosError(error)) {
-			throw error;
-		}
-		toast.error('Something unexpected happened.', toastConfig);
-	}
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: `Token ${token}`,
+            },
+        });
+        return response;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw error;
+        }
+        toast.error('Something unexpected happened.', toastConfig);
+    }
 };
 
 export const logoutUserAPI = async (token: string) => {
-	try {
-		await axios.post(
-			ENDPOINT_LOGOUT,
-			{},
-			{
-				headers: {
-					Authorization: `Token ${token}`,
-				},
-			}
-		);
-	} catch (error) {
-		toast.error('Something unexpected happened.', toastConfig);
-	}
+    try {
+        await axios.post(
+            ENDPOINT_LOGOUT,
+            {},
+            {
+                headers: {
+                    Authorization: `Token ${token}`,
+                },
+            }
+        );
+    } catch (error) {
+        toast.error('Something unexpected happened.', toastConfig);
+    }
+};
+
+export const updateUserAPI = async (token: string, data: UserUpdateForm) => {
+    try {
+        await axios.patch(ENDPOINT_PROFILE_EDIT, data, {
+            headers: { Authorization: `Token ${token}` },
+        });
+        toast.success('User information updated.', toastConfig);
+    } catch (error) {
+        toast.error('Could not update user information.', toastConfig);
+    }
 };
--- frontend/src/helpers/routes.ts
+++ frontend/src/helpers/routes.ts
@@ -4,12 +4,14 @@ export const ROUTE_CHAT = '/chat/';
 export const ROUTE_JOBS = '/jobs/';
 export const ROUTE_FEED = '/feed/';
 export const ROUTE_PROFILE = '/profile/';
+export const ROUTE_PROFILE_EDIT = '/profile/edit/';
 export const ROUTE_CONNECTIONS = '/connections/';
 export const ROUTE_NOTIFICATIONS = '/notifications/';
 
 //BACKEND ENDPOINT ROUTES
 export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
 export const ENDPOINT_PROFILE = 'http://localhost:8000/api/account/profile/';
+export const ENDPOINT_PROFILE_EDIT = 'http://localhost:8000/api/account/profile/edit/';
 export const ENDPOINT_LOGIN = 'http://localhost:8000/api/login/';
 export const ENDPOINT_LOGOUT = 'http://localhost:8000/api/auth/logout/';
 export const ENDPOINT_POST = 'http://localhost:8000/api/post/';
