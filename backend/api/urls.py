26b1a8cacbcf1d731b9ea99d1c88812e5b1581a5
jonksff <jonksff@gmail.com>
2023-08-30 15:45:49 -0400

    ADD: url endpoints to update avatar/banner

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,14 +4,20 @@ from . import views
 urlpatterns = [
     path("account/", views.UserCreationView.as_view(), name="register"),
     path(
-        "account/profile/",
-        views.UserRetrieveInformationView.as_view(),
-        name="profile",
+        "account/profile/", views.UserRetrieveInformationView.as_view(), name="profile"
     ),
     path(
-        "account/profile/edit/",
-        views.UserUpdateView.as_view(),
-        name="profile-update",
+        "account/profile/edit/", views.UserUpdateView.as_view(), name="profile-update"
+    ),
+    path(
+        "account/profile/edit/avatar/",
+        views.UserUpdateAvatarView.as_view(),
+        name="profile-avatar-update",
+    ),
+    path(
+        "account/profile/edit/banner/",
+        views.UserUpdateBannerView.as_view(),
+        name="profile-banner-update",
     ),
     path("login/", views.LoginView.as_view(), name="login"),
     path("auth/", include("knox.urls")),
