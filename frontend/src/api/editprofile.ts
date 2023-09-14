9180d5c14cd4476a8935793a91dc73d4d20413e1
jonksff <jonksff@gmail.com>
2023-09-14 10:50:57 -0400

    ADD: updateUserImage endpoint logic

--- frontend/src/api/editprofile.ts
+++ frontend/src/api/editprofile.ts
@@ -1,25 +1,43 @@
-import { ENDPOINT_PROFILE_EDIT } from '@/helpers/routes';
-import { toastConfig } from '@/helpers/toastifyConfig';
-import { UserUpdateForm } from '@/helpers/types';
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import { ENDPOINT_PROFILE_EDIT } from "@/helpers/routes";
+import { toastConfig } from "@/helpers/toastifyConfig";
+import { UpdateAvatarResponse, UserUpdateForm } from "@/helpers/types";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const updateUserAPI = async (token: string, data: UserUpdateForm) => {
-    try {
-        await axios.patch(ENDPOINT_PROFILE_EDIT, data, {
-            headers: { Authorization: `Token ${token}` },
-        });
-        toast.success('User information updated.', toastConfig);
-    } catch (error) {
-        toast.error('Could not update user information.', toastConfig);
-    }
+  try {
+    await axios.patch(ENDPOINT_PROFILE_EDIT, data, {
+      headers: { Authorization: `Token ${token}` },
+    });
+    toast.success("User information updated.", toastConfig);
+  } catch (error) {
+    toast.error("Could not update user information.", toastConfig);
+  }
 };
 
-export const updateAvatarAPI = async (image: File, token: string) => {
-    try {
-        console.log(image, token);
-        await axios.patch('123', {});
-    } catch (error) {
-        toast.error('Could not update avatar picture', toastConfig);
-    }
+export const updateUserImageAPI = async (
+  image: File,
+  token: string,
+  type: string
+) => {
+  try {
+    const url = `${ENDPOINT_PROFILE_EDIT}${type}/`;
+    const response = await axios.patch(
+      url,
+      {
+        avatar: image,
+      },
+      {
+        headers: {
+          Authorization: `Token ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    toast.success(`User ${type} updated.`, toastConfig);
+    return response.data as UpdateAvatarResponse;
+  } catch (error) {
+    toast.error(`Could not update ${type} picture.`, toastConfig);
+    throw error;
+  }
 };
