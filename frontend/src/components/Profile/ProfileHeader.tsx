88e4104f914667dc3aae9573b85e78bac00e04c5
jonksff <jonksff@gmail.com>
2023-09-19 11:33:51 -0400

    ADD: new component

---
+++ frontend/src/components/Profile/ProfileHeader.tsx
@@ -0,0 +1,94 @@
+import { ROUTE_PROFILE_EDIT } from '@/helpers/routes';
+import { ImageInformation, UserResponse } from '@/helpers/types';
+import { FiEdit2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+interface ProfileHeaderProps {
+	editable: boolean;
+	profile: UserResponse | undefined;
+	onEdit: () => void;
+	onSetInformation: React.Dispatch<React.SetStateAction<ImageInformation | undefined>>;
+}
+
+const ProfileHeader: React.FC<ProfileHeaderProps> = ({ editable, profile, onEdit, onSetInformation }) => {
+	return (
+		<>
+			<div className='relative'>
+				<img
+					src={profile?.banner}
+					alt=''
+					className='h-[200px] w-full bg-neutral-400 rounded-t-lg'
+				/>
+				{editable && (
+					<img
+						onClick={() => {
+							onSetInformation(() => {
+								if (!profile) return;
+
+								return {
+									type: 'avatar',
+									url: profile.avatar,
+								};
+							});
+							onEdit();
+						}}
+						src={profile?.avatar}
+						alt=''
+						className='w-[150px] h-[150px] rounded-full border-2 border-black absolute bottom-[-20%] left-5 hover:cursor-pointer'
+					/>
+				)}
+				{!editable && (
+					<img
+						src={profile?.avatar}
+						alt=''
+						className='w-[150px] h-[150px] rounded-full border-2 border-black absolute bottom-[-20%] left-5'
+					/>
+				)}
+				{editable && (
+					<button
+						className='absolute top-5 right-5 rounded-full bg-white p-2'
+						onClick={() => {
+							onSetInformation(() => {
+								if (!profile) return;
+
+								return {
+									type: 'banner',
+									url: profile.banner,
+								};
+							});
+							onEdit();
+						}}
+					>
+						<FiEdit2 size={20} />
+					</button>
+				)}
+			</div>
+			<div className='relative mt-10 p-4 flex flex-row gap-2 w-full'>
+				<div className='w-1/2'>
+					<p className='font-semibold'>{profile?.name}</p>
+					<p className='text-neutral-500 text-sm'>{profile?.title ? profile.title : 'No title set'}</p>
+					<p>{profile?.description ? profile.description : 'No description set'}</p>
+
+					<p className='text-blue-500 text-sm font-semibold mt-2'>{profile?.get_followers} contacts</p>
+				</div>
+				<div className='w-1/2'>
+					<p>{profile?.university ? profile.university : 'No university set'}</p>
+					<p>{profile?.actual_work ? profile.actual_work : 'No organization set'}</p>
+					<p className='text-neutral-400 text-sm'>
+						{profile?.location ? profile.location : 'No location set'}
+					</p>
+				</div>
+				{editable && (
+					<Link
+						className='absolute top-0 right-5 rounded-full bg-white p-2'
+						to={ROUTE_PROFILE_EDIT}
+					>
+						<FiEdit2 size={20} />
+					</Link>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default ProfileHeader;
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,33 +1,36 @@
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useCallback, useMemo, useState } from 'react';
 import CardContainer from '@/components/CardContainer';
 import { ImageInformation, UserResponse } from '@/helpers/types';
 import { getUserInformationAPI } from '@/api/auth';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { FiEdit2 } from 'react-icons/fi';
 import ConnectionsSummary from '@/components/Feed/ConnectionsSummary';
-import { ROUTE_PROFILE_EDIT } from '@/helpers/routes';
 import ProfileImageModal from '@/components/Modals/ProfileImageModal';
 import { uiActions } from '@/store/slices/ui-slice';
+import ProfileHeader from '@/components/Profile/ProfileHeader';
 
 const ProfilePage = () => {
 	const { userEmail } = useParams();
 	const userState = useAppSelector((state) => state.user);
 	const uiState = useAppSelector((state) => state.ui);
-	const [modalImageInformation, setmodalImageInformation] = useState<ImageInformation>();
+	const [modalImageInformation, setModalImageInformation] = useState<ImageInformation>();
 	const dispatch = useAppDispatch();
-
+	const [editable, setEditable] = useState(false);
 	const [profile, setProfile] = useState<UserResponse>();
 
 	useMemo(() => {
 		if (!userState.token || !userEmail) return;
+
+		if (userEmail === userState.email) {
+			setEditable(true);
+		}
 
 		void getUserInformationAPI(userState.token, userEmail)
 			.then((response) => {
 				setProfile(response?.data as UserResponse);
 			})
 			.catch((err) => console.log(err));
-	}, [userState.token, userEmail]);
+	}, [userState.token, userEmail, userState.email]);
 
 	const handleShowModal = useCallback(() => {
 		dispatch(uiActions.onShowUploadModal());
@@ -41,71 +44,12 @@ const ProfilePage = () => {
 		<div className='flex flex-row gap-4'>
 			<div className='basis-3/4'>
 				<CardContainer>
-					<div className='relative'>
-						<img
-							src={profile?.banner}
-							alt=''
-							className='h-[200px] w-full bg-neutral-400 rounded-t-lg'
-						/>
-						<img
-							onClick={() => {
-								setmodalImageInformation(() => {
-									if (!profile?.avatar) return;
-
-									return {
-										type: 'avatar',
-										url: profile.avatar,
-									};
-								});
-								handleShowModal();
-							}}
-							src={profile?.avatar}
-							alt=''
-							className='w-[150px] h-[150px] rounded-full border-2 border-black absolute bottom-[-20%] left-5 hover:cursor-pointer'
-						/>
-						<button
-							className='absolute top-5 right-5 rounded-full bg-white p-2'
-							onClick={() => {
-								setmodalImageInformation(() => {
-									if (!profile?.avatar) return;
-
-									return {
-										type: 'banner',
-										url: profile.banner,
-									};
-								});
-								handleShowModal();
-							}}
-						>
-							<FiEdit2 size={20} />
-						</button>
-					</div>
-					<div className='relative mt-10 p-4 flex flex-row gap-2 w-full'>
-						<div className='w-1/2'>
-							<p className='font-semibold'>{profile?.name}</p>
-							<p className='text-neutral-500 text-sm'>
-								{profile?.title ? profile.title : 'No title set'}
-							</p>
-							<p>{profile?.description ? profile.description : 'No description set'}</p>
-
-							<p className='text-blue-500 text-sm font-semibold mt-2'>
-								{profile?.get_followers} contacts
-							</p>
-						</div>
-						<div className='w-1/2'>
-							<p>{profile?.university ? profile.university : 'No university set'}</p>
-							<p>{profile?.actual_work ? profile.actual_work : 'No organization set'}</p>
-							<p className='text-neutral-400 text-sm'>
-								{profile?.location ? profile.location : 'No location set'}
-							</p>
-						</div>
-						<Link
-							className='absolute top-0 right-5 rounded-full bg-white p-2'
-							to={ROUTE_PROFILE_EDIT}
-						>
-							<FiEdit2 size={20} />
-						</Link>
-					</div>
+					<ProfileHeader
+						editable={editable}
+						profile={profile}
+						onEdit={handleShowModal}
+						onSetInformation={setModalImageInformation}
+					/>
 				</CardContainer>
 				<p>Latest posts</p>
 				<CardContainer>Body Posts</CardContainer>
@@ -115,11 +59,13 @@ const ProfilePage = () => {
 					<ConnectionsSummary />
 				</CardContainer>
 			</div>
-			<ProfileImageModal
-				isOpen={uiState.showUploadModal}
-				onClose={handleCloseModal}
-				imageInfo={modalImageInformation}
-			/>
+			{editable && (
+				<ProfileImageModal
+					isOpen={uiState.showUploadModal}
+					onClose={handleCloseModal}
+					imageInfo={modalImageInformation}
+				/>
+			)}
 		</div>
 	);
 };
