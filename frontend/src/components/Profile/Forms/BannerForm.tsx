81846202fc6ae8ba29f2cb95bb01f7de1e0246a0
jonksff <jonksff@gmail.com>
2023-09-21 11:30:29 -0400

    EDIT: logic

--- frontend/src/components/Profile/Forms/BannerForm.tsx
+++ frontend/src/components/Profile/Forms/BannerForm.tsx
@@ -1,13 +1,48 @@
+import { updateUserImageAPI } from '@/api/editprofile';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { uiActions } from '@/store/slices/ui-slice';
+import { useCallback, useState } from 'react';
+
 const BannerForm = () => {
-    return (
-        <form className='w-full flex flex-col'>
-            <input type='file' />
-            <div className='flex flex-row justify-end gap-2'>
-                <button type='button'>Cancel</button>
-                <button type='submit'>Save</button>
-            </div>
-        </form>
-    );
+	const [newImage, setNewImage] = useState<File | undefined>(undefined);
+	const userState = useAppSelector((state) => state.user);
+	const dispatch = useAppDispatch();
+
+	const submitHandler = useCallback(
+		async (e: React.FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+			if (!newImage || !userState.token) return;
+			try {
+				const response = await updateUserImageAPI(newImage, userState.token, 'banner');
+				dispatch(userActions.updateAvatar(response.banner));
+				dispatch(uiActions.onCloseUploadModal());
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		[newImage, userState.token, dispatch]
+	);
+
+	const imageChangeHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		if (!e.target.files) return;
+		setNewImage(e.target.files[0]);
+	}, []);
+
+	return (
+		<form
+			className='w-full flex flex-col'
+			onSubmit={submitHandler}
+		>
+			<input
+				type='file'
+				onChange={imageChangeHandler}
+			/>
+			<div className='flex flex-row justify-end gap-2'>
+				<button type='button'>Cancel</button>
+				<button type='submit'>Save</button>
+			</div>
+		</form>
+	);
 };
 
 export default BannerForm;
