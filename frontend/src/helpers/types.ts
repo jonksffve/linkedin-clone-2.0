535f8b84fadb60072b568bb0da0aa866050ae4b3
jonksff <jonksff@gmail.com>
2023-09-25 11:20:33 -0400

    EDIT: update avatar response types

--- frontend/src/helpers/types.ts
+++ frontend/src/helpers/types.ts
@@ -1,114 +1,115 @@
 //* FORMS
 export interface LoginFormInputs {
-  username: string;
-  password: string;
+	username: string;
+	password: string;
 }
 
 export interface RegisterFormInputs {
-  email: string;
-  password: string;
-  first_name: string;
-  last_name: string;
-  avatar: File | undefined;
+	email: string;
+	password: string;
+	first_name: string;
+	last_name: string;
+	avatar: File | undefined;
 }
 
 export interface CreatePostFormInputs {
-  content: string;
-  file: File | undefined;
+	content: string;
+	file: File | undefined;
 }
 
 export interface CommentFormInput {
-  content: string;
+	content: string;
 }
 
 export interface UserUpdateForm {
-  first_name?: string;
-  last_name?: string;
-  title?: string;
-  description?: string;
-  university?: string;
-  actual_work?: string;
-  location?: string;
+	first_name?: string;
+	last_name?: string;
+	title?: string;
+	description?: string;
+	university?: string;
+	actual_work?: string;
+	location?: string;
 }
 
 //! END FORMS
 
 export interface UserState {
-  token: string | undefined;
-  id: number | undefined;
-  first_name: string;
-  last_name: string;
-  email: string;
-  avatar: string;
-  banner: string;
-  title: string;
-  description: string;
-  university: string;
-  actual_work: string;
-  location: string;
-  name: string;
-  get_followers: number;
-  get_following: number;
-  get_posts: number;
-  logged: boolean;
+	token: string | undefined;
+	id: number | undefined;
+	first_name: string;
+	last_name: string;
+	email: string;
+	avatar: string;
+	banner: string;
+	title: string;
+	description: string;
+	university: string;
+	actual_work: string;
+	location: string;
+	name: string;
+	get_followers: number;
+	get_following: number;
+	get_posts: number;
+	logged: boolean;
 }
 
 //* API RESPONSES
 export interface UserResponse {
-  id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  avatar: string;
-  banner: string;
-  title: string;
-  description: string;
-  university: string;
-  actual_work: string;
-  location: string;
-  name: string;
-  get_followers: number;
-  get_following: number;
-  get_posts: number;
+	id: number;
+	first_name: string;
+	last_name: string;
+	email: string;
+	avatar: string;
+	banner: string;
+	title: string;
+	description: string;
+	university: string;
+	actual_work: string;
+	location: string;
+	name: string;
+	get_followers: number;
+	get_following: number;
+	get_posts: number;
 }
 
 export interface TokenResponse {
-  token: string;
-  user: UserResponse;
+	token: string;
+	user: UserResponse;
 }
 
 export interface ErrorResponse {
-  key: string;
-  value: string;
+	key: string;
+	value: string;
 }
 
 export interface Post {
-  id: string;
-  user: UserResponse;
-  content: string;
-  date_created: string;
-  file: string | undefined;
-  is_liked: boolean;
-  get_comments: number;
-  get_likes: number;
+	id: string;
+	user: UserResponse;
+	content: string;
+	date_created: string;
+	file: string | undefined;
+	is_liked: boolean;
+	get_comments: number;
+	get_likes: number;
 }
 
 export interface Comment {
-  id: string;
-  post: string;
-  user: UserResponse;
-  content: string;
-  date_created: string;
-  parent: string | undefined;
-  replies_count: number;
-  is_liked: boolean;
+	id: string;
+	post: string;
+	user: UserResponse;
+	content: string;
+	date_created: string;
+	parent: string | undefined;
+	replies_count: number;
+	is_liked: boolean;
 }
 
 export interface ImageInformation {
-  type: string;
-  url: string;
+	type: string;
+	url: string;
 }
 
 export interface UpdateAvatarResponse {
-  avatar: string;
+	avatar: string | undefined;
+	banner: string | undefined;
 }
